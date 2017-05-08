68d4fd942769b397d5eda289d5d96470335fe394
NAT <gotheap@hotmail.com>
2017-05-08 20:31:54 +0700

    df

--- PointerToPointer/PointerToPointer.cpp
+++ PointerToPointer/PointerToPointer.cpp
@@ -69,15 +69,46 @@ bool AddTail(Node* pHead, int data)
 			while (pTail->pNext != NULL)
 			{
 				pTail = pTail->pNext;
-				if (pTail->pNext == NULL)
-				{
-					pTail->pNext = pNode;
-					return true;
-				}
+				//if (pTail->pNext == NULL)
+				//{
+				//	pTail->pNext = pNode;
+				//	//return true;
+				//}
 
 			}
+			pTail->pNext = pNode;
 		}
 
+	}
+}
+void DeleteTail(Node* &pHead) 
+{
+	Node* pTail = NULL;
+	pTail = pHead;
+	Node* pTailNext = pTail->pNext;
+	if (pTail -> pNext == NULL)
+	{
+		delete pHead;
+	}
+	else
+	{
+		while (pTailNext -> pNext != NULL)
+		{
+			pTail = pTail->pNext;
+			pTailNext = pTailNext-> pNext;
+		}
+		delete pTailNext;
+		pTail->pNext = NULL;
+	}
+}
+
+void PrintList(Node* pHead)
+{
+	Node* pNode = pHead;
+	while (pNode != NULL)
+	{
+		cout << pNode->key;
+		pNode = pNode->pNext;
 	}
 }
 //bool AddAfter(Node* &pHead, int data)
@@ -91,12 +122,17 @@ int main()
 {
 	Node *pRoot;
 	pRoot = NULL;
-	for (int i = 1; i < 10; i++)
+	for (int i = 1; i < 3; i++)
 	{
 		AddHead(pRoot, i);
 	}
 	AddTail(pRoot, 3);
 	AddHead(pRoot, 2);
+	PrintList(pRoot);
+	cout << "\n";
+	DeleteTail(pRoot);
+	PrintList(pRoot);
+
 
 	return 0;
 }
