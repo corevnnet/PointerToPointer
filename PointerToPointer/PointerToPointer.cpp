2e0ce72370803b286774ec0da58e7d62b49e7460
NAT <gotheap@hotmail.com>
2017-05-09 02:36:55 +0700

    fn

--- PointerToPointer/PointerToPointer.cpp
+++ PointerToPointer/PointerToPointer.cpp
@@ -101,38 +101,124 @@ void DeleteTail(Node* &pHead)
 		pTail->pNext = NULL;
 	}
 }
-
 void PrintList(Node* pHead)
 {
 	Node* pNode = pHead;
-	while (pNode != NULL)
+	if (pNode == NULL)
 	{
-		cout << pNode->key;
-		pNode = pNode->pNext;
+		cout << "Danh sach nay rong";
+	}
+	else
+	{
+		while (pNode != NULL)
+		{
+			cout << pNode->key<<"&#$";
+			pNode = pNode->pNext;
+		}
 	}
 }
-//bool AddAfter(Node* &pHead, int data)
+
+void DeleteAll(Node* &pHead)
+{
+	Node* pNode = NULL;
+	pNode = pHead;
+	Node* pNodeNext = pNode->pNext;
+	if (pNode -> pNext == NULL)
+	{
+		delete pHead;
+	}
+	else
+	{
+		while (pHead -> pNext!=NULL)
+		{
+			PrintList(pHead);
+			cout << "\n";
+			pNode = pHead;
+			pNodeNext = pNode->pNext;
+			while (pNodeNext -> pNext !=NULL)
+			{
+				pNode = pNode->pNext;
+				pNodeNext = pNodeNext->pNext;
+
+			}
+			delete pNodeNext;
+			pNode->pNext = NULL;
+			
+
+		}
+		delete pNode;
+		pHead = NULL;
+	}
+}
+bool RemoveMiddle(Node* pHead, int key)
+{
+	Node* pCur = NULL;
+	Node* pCurNext;
+	pCur = pHead;
+	
+	if (pCur -> pNext == NULL)
+	{
+		return false;
+	}
+	else
+	{
+		pCurNext = pCur->pNext;
+	}
+	while (pCurNext -> key != key)
+	{
+		pCur = pCur->pNext;
+		pCurNext = pCurNext->pNext;
+		if (pCurNext -> key == key)
+		{
+			pCur->pNext = pCurNext->pNext;
+			delete pCurNext;
+			return true;
+		}
+	}
+}
+//bool RemoveHead(Node* &pHead)
 //{
-//	Node* pNode = CreateNode(data);
-//	
+//	Node* pNode = NULL;
+//	pNode = pHead;
+//
 //}
+//void DnguocdS(Node* &pHead)
+//{
+//	Node* pNode = NULL;
+//	Node* pNodeNext = NULL;
+//	pNode = pHead;
+//	pNodeNext = pNode->pNext;
+//	Node* pHeadTmp;
+//	while (pNode -> pNext != NULL)
+//	{
+//		pNode = pNode->pNext;
+//		pno
+//	}
+//
+//}
+
 
 
 int main()
 {
 	Node *pRoot;
 	pRoot = NULL;
-	for (int i = 1; i < 3; i++)
+	for (int i = 1; i < 77; i++)
 	{
 		AddHead(pRoot, i);
 	}
-	AddTail(pRoot, 3);
-	AddHead(pRoot, 2);
+	/*AddTail(pRoot, 3);
+	AddHead(pRoot, 2);*/
 	PrintList(pRoot);
 	cout << "\n";
-	DeleteTail(pRoot);
+	//RemoveMiddle(pRoot, 5);
+	DeleteAll(pRoot);
+	cout << "Sau khi xoa \n";
 	PrintList(pRoot);
-
+	cout << "\n";
+	/*DeleteTail(pRoot);
+	PrintList(pRoot);
+*/
 
 	return 0;
 }
